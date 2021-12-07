c7e455c6de894a59afbb03c42da3aef2fe849bc2
Robert Turner <robturneris@gmail.com>
2021-12-06 22:21:04 -0500

    Converted App component to functional

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import Main from "./components/MainComponent";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
