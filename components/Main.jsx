e0f627a4ac3b5393b080c7502cd271026cc5af73
Robert Turner <robturneris@gmail.com>
2021-12-07 21:42:07 -0500

    Removed component and related route

--- components/Main.jsx
+++ components/Main.jsx
@@ -16,19 +16,6 @@ class Main extends Component {
       return <Home />;
     };
 
-    const CampsiteWithId = ({ match }) => {
-      return (
-        <CampsiteInfo
-          campsite={this.props.campsites.campsites.filter((campsite) => campsite.id === +match.params.campsiteId)[0]}
-          isLoading={this.props.campsites.isLoading}
-          errMess={this.props.campsites.errMess}
-          comments={this.props.comments.comments.filter((comment) => comment.campsiteId === +match.params.campsiteId)}
-          commentsErrMess={this.props.comments.errMess}
-          postComment={this.props.postComment}
-        />
-      );
-    };
-
     return (
       <div>
         <Header />
@@ -37,7 +24,6 @@ class Main extends Component {
             <Switch>
               <Route exact path="/" component={HomePage} />
               <Route exact path="/directory" render={() => <Directory campsites={this.props.campsites} />} />
-              <Route path="/directory/:campsiteId" component={CampsiteWithId} />
               <Route exact path="/contact" render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} postFeedback={this.props.postFeedback} />} />
               <Route exact path="/about" render={() => <About partners={this.props.partners} isLoading={this.props.partners.isLoading} ErrMess={this.props.partners.errMess} />} />
               <Redirect to="/" />
