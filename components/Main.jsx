5ea1856edd54cc015d572674c7fb694243e46987
Robert Turner <robturneris@gmail.com>
2021-12-07 22:16:32 -0500

    Converted Main component to functional

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
@@ -6,27 +6,25 @@ import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 
-class Main extends Component {
-  componentDidMount() {}
+const Main = () => {
+  componentDidMount();
 
-  render() {
-    const HomePage = () => {
-      return <Home />;
-    };
+  const HomePage = () => {
+    return <Home />;
+  };
 
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/contact" render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} postFeedback={this.props.postFeedback} />} />
-          <Route exact path="/about" render={() => <About partners={this.props.partners} isLoading={this.props.partners.isLoading} ErrMess={this.props.partners.errMess} />} />
-          <Redirect to="/" />
-        </Switch>
-        <Footer />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/contact" render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} postFeedback={this.props.postFeedback} />} />
+        <Route exact path="/about" render={() => <About partners={this.props.partners} isLoading={this.props.partners.isLoading} ErrMess={this.props.partners.errMess} />} />
+        <Redirect to="/" />
+      </Switch>
+      <Footer />
+    </div>
+  );
+};
 
 export default withRouter(Main);
