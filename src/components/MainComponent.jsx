b613ecfa86b9e285a22117421ce7091b82f1aa2f
Robert Turner <robturneris@gmail.com>
2021-12-08 23:46:27 -0500

    Moved components directory to src directory

---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+
+const Main = () => {
+  componentDidMount();
+
+  const HomePage = () => {
+    return <Home />;
+  };
+
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/contact" render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} postFeedback={this.props.postFeedback} />} />
+        <Route exact path="/about" render={() => <About partners={this.props.partners} isLoading={this.props.partners.isLoading} ErrMess={this.props.partners.errMess} />} />
+        <Redirect to="/" />
+      </Switch>
+      <Footer />
+    </div>
+  );
+};
+
+export default withRouter(Main);
