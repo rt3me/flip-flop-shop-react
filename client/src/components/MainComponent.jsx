099e7aa58619ed6edf872681145e8b0540c9b6bd
Robert Turner <robturneris@gmail.com>
2022-01-06 21:11:31 -0500

    Removed unused components

--- client/src/components/MainComponent.jsx
+++ client/src/components/MainComponent.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import Directory from "./DirectoryComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
-import CampsiteInfo from "./CampsiteInfoComponent";
 import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
@@ -58,19 +56,6 @@ class Main extends Component {
       );
     };
 
-    const CampsiteWithId = ({ match }) => {
-      return (
-        <CampsiteInfo
-          campsite={this.props.campsites.campsites.filter((campsite) => campsite.id === +match.params.campsiteId)[0]}
-          isLoading={this.props.campsites.isLoading}
-          errMess={this.props.campsites.errMess}
-          comments={this.props.comments.comments.filter((comment) => comment.campsiteId === +match.params.campsiteId)}
-          commentsErrMess={this.props.comments.errMess}
-          postComment={this.props.postComment}
-        />
-      );
-    };
-
     return (
       <div>
         <Header />
@@ -78,7 +63,6 @@ class Main extends Component {
           <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
             <Switch>
               <Route exact path="/" component={HomePage} />
-              <Route path="/directory/:campsiteId" component={CampsiteWithId} />
               <Route exact path="/contact" render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} postFeedback={this.props.postFeedback} />} />
               <Route exact path="/about" render={() => <About partners={this.props.partners} isLoading={this.props.partners.isLoading} ErrMess={this.props.partners.errMess} />} />
               <Redirect to="/" />
