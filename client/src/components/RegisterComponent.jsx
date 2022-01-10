b02743a0fa5b801f42288629de3b634eb82daccd
Robert Turner <robturneris@gmail.com>
2022-01-10 15:34:58 -0500

    Added registration form

--- client/src/components/RegisterComponent.jsx
+++ client/src/components/RegisterComponent.jsx
@@ -1,11 +1,16 @@
-import React from "react";
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from "reactstrap";
+import React, { useState } from "react";
+import { Breadcrumb, BreadcrumbItem, Form, InputGroup, InputGroupText, Input, Button } from "reactstrap";
 import { Link } from "react-router-dom";
-import { Fade, Stagger } from "react-animation-components";
-import { Loading } from "./LoadingComponent";
-import { baseUrl } from "../shared/baseUrl";
 
-function Register(props) {
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = () => {
+    console.log(name, email, password);
+  };
+
   return (
     <div className="container">
       <div className="row">
@@ -14,42 +19,48 @@ function Register(props) {
             <BreadcrumbItem>
               <Link to="/home">Home</Link>
             </BreadcrumbItem>
-            <BreadcrumbItem active>About Us</BreadcrumbItem>
+            <BreadcrumbItem active>Register</BreadcrumbItem>
           </Breadcrumb>
-          <h2>About Us</h2>
+          <h2>Register</h2>
           <hr />
         </div>
       </div>
-      <div className="row row-content">
-        <div className="col-sm-6">
-          <h3>Our Mission</h3>
-          <p>
-            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert
-            wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-          </p>
+      <div className="row">
+        <div className="col">
+          <h3>Let's Get Started</h3>
+          <p>Sign up for free. No credit card required.</p>
+          <Form>
+            <InputGroup>
+              <InputGroupText>Name</InputGroupText>
+              <Input type="text" onChange={(e) => setName(e.target.value)} value={name} id="nameInput" name="name" placeholder="name" />
+            </InputGroup>
+            <br />
+            <InputGroup>
+              <InputGroupText>Email</InputGroupText>
+              <Input type="email" onChange={(e) => setEmail(e.target.value)} value={email} id="emailInput" name="email" placeholder="user@email.com" />
+            </InputGroup>
+            <br />
+            <InputGroup>
+              <InputGroupText>Password</InputGroupText>
+              <Input type="password" onChange={(e) => setPassword(e.target.value)} value={password} id="passwordInput" name="password" placeholder="password" />
+            </InputGroup>
+            <br />
+            <Button onClick={handleClick} color="primary">
+              Register
+            </Button>
+          </Form>
+          <br />
+          <br />
+          <br />
         </div>
-        <div className="col-sm-6">
-          <Card>
-            <CardHeader className="text-white bg-primary">
-              <h3>Facts At a Glance</h3>
-            </CardHeader>
-            <CardBody>
-              <dl className="row">
-                <dt className="col-6">Founded</dt>
-                <dd className="col-6">February 3, 2016</dd>
-                <dt className="col-6">No. of Campsites in 2019</dt>
-                <dd className="col-6">563</dd>
-                <dt className="col-6">No. of Reviews in 2019</dt>
-                <dd className="col-6">4388</dd>
-                <dt className="col-6">Employees</dt>
-                <dd className="col-6">42</dd>
-              </dl>
-            </CardBody>
-          </Card>
+      </div>
+      <div className="row">
+        <div className="col">
+          <pre>{JSON.stringify({ name, email, password }, null, 4)}</pre>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Register;
