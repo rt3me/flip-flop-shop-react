2999aa0132a0fcf4f232d6dfa42b63879b8a1987
Robert Turner <robturneris@gmail.com>
2022-01-11 00:46:32 -0500

    Added user schema model

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+    max: 25,
+  },
+});
+
+export default mongoose.model("User", userSchema);
