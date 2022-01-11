de3ce26209a9f6e71a0e4ec04b42f57069364584
Robert Turner <robturneris@gmail.com>
2022-01-10 23:48:32 -0500

    Async, try catch in register controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,7 @@
-export const register = (req, res) => {
-  res.json({
-    data: "You have reached the node server: controller",
-  });
+export const register = async (req, res) => {
+  try {
+    // validation
+  } catch (err) {
+    console.log(err);
+  }
 };
