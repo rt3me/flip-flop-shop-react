823c6e76ddb2ead6e7afdd7ea19892e61fb8ffca
Robert Turner <robturneris@gmail.com>
2022-01-10 23:57:21 -0500

    Destructure name, password from request body

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ export const register = async (req, res) => {
   try {
     // validation
     console.log(req.body);
+    const { name, password } = req.body;
   } catch (err) {
     console.log(err);
   }
