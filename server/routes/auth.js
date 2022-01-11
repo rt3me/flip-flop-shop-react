af79cb542899bf0094c285d0a69c86603354b7f6
Robert Turner <robturneris@gmail.com>
2022-01-10 23:35:00 -0500

    JSON response from router

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,9 @@ import express from "express";
 const router = express.Router();
 
 router.get("/register", (req, res) => {
-  res.send("You have reached the node server");
+  res.json({
+    data: "You have reached the node server",
+  });
 });
 
 module.exports = router;
