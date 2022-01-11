8248d79ba0103c4739a9047beb6566eea76c3b4e
Robert Turner <robturneris@gmail.com>
2022-01-11 01:23:41 -0500

    Save user if user email does not exist

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,11 @@
+import User from "models/user";
+import { hashPassword, comparePassword } from "../helpers/auth";
+
 export const register = async (req, res) => {
   try {
     // validation
     console.log(req.body);
-    const { name, password } = req.body;
+    const { name, email, password } = req.body;
     if (!name) {
       return res.json({
         error: "Name is required",
@@ -13,9 +16,26 @@ export const register = async (req, res) => {
         error: "Password is required and should be at least 8 characters",
       });
     }
-    res.json({
-      data: "/register endpoint",
-    });
+    const exist = await User.find({ email });
+    if (exist) {
+      return res.json({
+        error: "Email is already in use",
+      });
+    }
+    // hash password
+    const hashedPassword = hashPassword(password);
+    try {
+      const user = await new User({
+        name,
+        email,
+        password: hashedPassword,
+      }).save();
+
+      console.log(user);
+      const { password, ...rest } = user._doc;
+    } catch (err) {
+      console.log(err);
+    }
   } catch (err) {
     console.log(err);
   }
