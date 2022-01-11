8052b097e6b5576cae0dee1369f47bf9842ed6fe
Robert Turner <robturneris@gmail.com>
2022-01-10 23:59:23 -0500

    Require name error message

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,6 +3,11 @@ export const register = async (req, res) => {
     // validation
     console.log(req.body);
     const { name, password } = req.body;
+    if (!name) {
+      return res.json({
+        error: "Name is required",
+      });
+    }
   } catch (err) {
     console.log(err);
   }
