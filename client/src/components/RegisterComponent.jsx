279e3f94f7dd32da46b64662ede84eff729ed324
Robert Turner <robturneris@gmail.com>
2022-01-11 22:14:26 -0500

    No props parameter

--- client/src/components/RegisterComponent.jsx
+++ client/src/components/RegisterComponent.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const Register = (props) => {
+const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
