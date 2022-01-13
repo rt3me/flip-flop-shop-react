56e12f8221c25bc56bfab5f4e699649c3759cf1c
Robert Turner <robturneris@gmail.com>
2022-01-13 01:31:00 -0500

    useState, useEffect, and axios config

---
+++ client/src/context/index.js
@@ -0,0 +1,20 @@
+import { useState, useEffect, createContext } from "react";
+import axios from "axios";
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [state, setState] = useState({
+    user: {},
+    token: "",
+  });
+
+  useEffect(() => {
+    setState(JSON.parse(localStorage.getItem("auth")));
+  }, []);
+
+  // axios config
+  const token = state && state.token ? state.token : "";
+  axios.defaults.baseURL = process.env.REACT_APP_API;
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+};
