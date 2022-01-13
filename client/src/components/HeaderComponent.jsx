233d2136358ed48cce0bd865c0bdf000c4cf8971
Robert Turner <robturneris@gmail.com>
2022-01-12 23:00:50 -0500

    Converted Home component to functional

--- client/src/components/HeaderComponent.jsx
+++ client/src/components/HeaderComponent.jsx
@@ -1,12 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from "reactstrap";
 import { NavLink, useNavigate } from "react-router-dom";
 import { isAuth } from "../utils/functions";
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-
+function Header(props) {
+  function constructor(props) {
     this.state = {
       isNavOpen: false,
       isModalOpen: false,
@@ -17,104 +15,102 @@ class Header extends Component {
     this.handleLogin = this.handleLogin.bind(this);
   }
 
-  navigate = useNavigate();
+  const navigate = useNavigate();
 
-  toggleNav() {
+  function toggleNav() {
     this.setState({
       isNavOpen: !this.state.isNavOpen,
     });
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <div className="p-5 bg-primary text-white jumbotron">
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <h1>NuCamp</h1>
-                <h2>a better way to camp</h2>
-              </div>
+  return (
+    <React.Fragment>
+      <div className="p-5 bg-primary text-white jumbotron">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1>NuCamp</h1>
+              <h2>a better way to camp</h2>
             </div>
           </div>
         </div>
-        <Navbar dark sticky="top" expand="md">
-          <div className="container">
-            <NavbarBrand className="mr-auto" href="/">
-              <img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" />
-            </NavbarBrand>
-            <NavbarToggler onClick={this.toggleNav} />
-            <Collapse isOpen={this.state.isNavOpen} navbar>
-              <Nav navbar>
-                <NavItem>
-                  <NavLink className="nav-link" to="/">
-                    <i className="fa fa-home fa-lg" /> Home
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/about">
-                    <i className="fa fa-info fa-lg" /> About
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/contact">
-                    <i className="fa fa-address-card fa-lg" /> Contact Us
-                  </NavLink>
-                </NavItem>
+      </div>
+      <Navbar dark sticky="top" expand="md">
+        <div className="container">
+          <NavbarBrand className="mr-auto" href="/">
+            <img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" />
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggleNav} />
+          <Collapse isOpen={this.state.isNavOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink className="nav-link" to="/">
+                  <i className="fa fa-home fa-lg" /> Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/about">
+                  <i className="fa fa-info fa-lg" /> About
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/contact">
+                  <i className="fa fa-address-card fa-lg" /> Contact Us
+                </NavLink>
+              </NavItem>
 
-                {isAuth() ? (
-                  <React.Fragment>
-                    <NavItem>
-                      <NavLink className="nav-link" to="/login">
-                        <i className="fa fa-sign-in fa-lg" /> Username / Logout
-                      </NavLink>
-                    </NavItem>
-                  </React.Fragment>
-                ) : (
-                  <React.Fragment>
-                    <NavItem>
-                      <NavLink className="nav-link" to="/login">
-                        <i className="fa fa-sign-in fa-lg" /> Register
-                      </NavLink>
-                    </NavItem>
-                    <NavItem>
-                      <NavLink className="nav-link" to="/login">
-                        <i className="fa fa-sign-in fa-lg" /> Login
-                      </NavLink>
-                    </NavItem>
-                  </React.Fragment>
-                )}
-              </Nav>
-            </Collapse>
-          </div>
-        </Navbar>
-        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
-          <ModalBody>
-            <Form onSubmit={this.handleLogin}>
-              <FormGroup>
-                <Label htmlFor="username">Username</Label>
-                <Input type="text" id="username" name="username" innerRef={(input) => (this.username = input)} />
-              </FormGroup>
-              <FormGroup>
-                <Label htmlFor="password">Password</Label>
-                <Input type="password" id="password" name="password" innerRef={(input) => (this.password = input)} />
-              </FormGroup>
-              <FormGroup check>
-                <Label check>
-                  <Input type="checkbox" name="remember" innerRef={(input) => (this.remember = input)} />
-                  Remember me
-                </Label>
-              </FormGroup>
-              <Button type="submit" value="submit" color="primary">
-                Login
-              </Button>
-            </Form>
-          </ModalBody>
-        </Modal>
-      </React.Fragment>
-    );
-  }
+              {isAuth() ? (
+                <React.Fragment>
+                  <NavItem>
+                    <NavLink className="nav-link" to="/login">
+                      <i className="fa fa-sign-in fa-lg" /> Username / Logout
+                    </NavLink>
+                  </NavItem>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <NavItem>
+                    <NavLink className="nav-link" to="/login">
+                      <i className="fa fa-sign-in fa-lg" /> Register
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="nav-link" to="/login">
+                      <i className="fa fa-sign-in fa-lg" /> Login
+                    </NavLink>
+                  </NavItem>
+                </React.Fragment>
+              )}
+            </Nav>
+          </Collapse>
+        </div>
+      </Navbar>
+      <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+        <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+        <ModalBody>
+          <Form onSubmit={this.handleLogin}>
+            <FormGroup>
+              <Label htmlFor="username">Username</Label>
+              <Input type="text" id="username" name="username" innerRef={(input) => (this.username = input)} />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="password">Password</Label>
+              <Input type="password" id="password" name="password" innerRef={(input) => (this.password = input)} />
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="checkbox" name="remember" innerRef={(input) => (this.remember = input)} />
+                Remember me
+              </Label>
+            </FormGroup>
+            <Button type="submit" value="submit" color="primary">
+              Login
+            </Button>
+          </Form>
+        </ModalBody>
+      </Modal>
+    </React.Fragment>
+  );
 }
 
 export default Header;
