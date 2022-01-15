402356b7361eb723911d80ac51026acd74ae0c70
Robert Turner <robturneris@gmail.com>
2022-01-15 01:32:24 -0500

    Load images with no baseUrl

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -1,10 +1,10 @@
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Button } from "reactstrap";
 import { baseUrl } from "../shared/baseUrl";
 
-const CardComponent = ({ cardImage = "/images/redux-woods.jpg", cardBodyTitle = "Frugal", cardBodySubtitle = "$10/mo", cardBodyText, buttonText = "Sign Up", outline }) => {
+const CardComponent = ({ cardImage = "../../images/flip-flop-frugal.svg", cardBodyTitle = "Frugal", cardBodySubtitle = "$10/mo", cardBodyText, buttonText = "Sign Up", outline }) => {
   return (
-    <Card className="mb-5 mb-md-0">
-      <CardImg src={baseUrl + cardImage} alt={cardBodyTitle} />
+    <Card className={outline ? "mb-5 mb-md-0" : "mb-5 mb-md-0 border-0"}>
+      <CardImg src={cardImage} alt={cardBodyTitle} />
       <CardBody>
         <CardTitle tag="h3">{cardBodyTitle}</CardTitle>
         <CardSubtitle tag="h4" className="mb-2 text-muted">
