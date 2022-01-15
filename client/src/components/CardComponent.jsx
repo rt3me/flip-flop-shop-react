d1d96aaf6abc91b702f68bff11f85656727f1b70
Robert Turner <robturneris@gmail.com>
2022-01-14 23:10:01 -0500

    Card titles h3

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -1,12 +1,12 @@
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 import { baseUrl } from "../shared/baseUrl";
 
-const CardComponent = ({ cardImage = "/images/redux-woods.jpg", cardName = "BASIC", cardDescription = "Default description" }) => {
+const CardComponent = ({ cardImage = "/images/redux-woods.jpg", cardName = "CARD TITLE", cardDescription = "Default description" }) => {
   return (
     <Card className="mb-5 mb-md-0">
       <CardImg src={baseUrl + cardImage} alt={cardName} />
       <CardBody>
-        <CardTitle>{cardName}</CardTitle>
+        <CardTitle tag="h3">{cardName}</CardTitle>
         <CardText>{cardDescription}</CardText>
       </CardBody>
     </Card>
