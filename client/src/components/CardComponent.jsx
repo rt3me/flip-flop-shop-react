0468ef31da4c8fbb25647829ecd1667122aeaab3
Robert Turner <robturneris@gmail.com>
2022-01-15 02:10:09 -0500

    Smaller card images

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -4,7 +4,7 @@ import { baseUrl } from "../shared/baseUrl";
 const CardComponent = ({ cardImage = "../../images/flip-flop-frugal.svg", cardBodyTitle = "Frugal", cardBodySubtitle = "$10/mo", cardBodyText, buttonText = "Sign Up", outline }) => {
   return (
     <Card className={outline ? "mb-5 mb-md-0" : "mb-5 mb-md-0 border-0"}>
-      <CardImg src={cardImage} alt={cardBodyTitle} />
+      <CardImg src={cardImage} alt={cardBodyTitle} className="w-50 m-auto" />
       <CardBody>
         <CardTitle tag="h3">{cardBodyTitle}</CardTitle>
         <CardSubtitle tag="h4" className="mb-2 text-muted">
