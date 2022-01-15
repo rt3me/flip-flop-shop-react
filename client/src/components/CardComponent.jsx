a249a0435890a2c97e11f5828a034e66225d0147
Robert Turner <robturneris@gmail.com>
2022-01-14 23:20:42 -0500

    Rename title prop

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -1,12 +1,12 @@
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from "reactstrap";
 import { baseUrl } from "../shared/baseUrl";
 
-const CardComponent = ({ cardImage = "/images/redux-woods.jpg", cardName = "CARD TITLE", cardBodySubtitle = "Default description" }) => {
+const CardComponent = ({ cardImage = "/images/redux-woods.jpg", cardBodyTitle = "CARD TITLE", cardBodySubtitle = "Default description" }) => {
   return (
     <Card className="mb-5 mb-md-0">
-      <CardImg src={baseUrl + cardImage} alt={cardName} />
+      <CardImg src={baseUrl + cardImage} alt={cardBodyTitle} />
       <CardBody>
-        <CardTitle tag="h3">{cardName}</CardTitle>
+        <CardTitle tag="h3">{cardBodyTitle}</CardTitle>
         <CardSubtitle tag="h4" className="mb-2 text-muted">
           {cardBodySubtitle}
         </CardSubtitle>
