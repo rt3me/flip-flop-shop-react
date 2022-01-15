31e242a566c5ab9f04f65e670b6c6fe4cea2fdb2
Robert Turner <robturneris@gmail.com>
2022-01-14 23:15:30 -0500

    Added card subtitle

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -1,12 +1,15 @@
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from "reactstrap";
 import { baseUrl } from "../shared/baseUrl";
 
-const CardComponent = ({ cardImage = "/images/redux-woods.jpg", cardName = "CARD TITLE", cardDescription = "Default description" }) => {
+const CardComponent = ({ cardImage = "/images/redux-woods.jpg", cardName = "CARD TITLE", cardBodySubtitle = "Default description" }) => {
   return (
     <Card className="mb-5 mb-md-0">
       <CardImg src={baseUrl + cardImage} alt={cardName} />
       <CardBody>
         <CardTitle tag="h3">{cardName}</CardTitle>
+        <CardSubtitle tag="h4" className="mb-2 text-muted">
+          {cardBodySubtitle}
+        </CardSubtitle>
         <CardText>{cardDescription}</CardText>
       </CardBody>
     </Card>
