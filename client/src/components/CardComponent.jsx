3e349f0ac2aa974bd5e17c6bbe8fa5328457d09a
Robert Turner <robturneris@gmail.com>
2022-01-16 00:26:49 -0500

    Added handleClick prop

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -1,7 +1,7 @@
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Button } from "reactstrap";
 import { baseUrl } from "../shared/baseUrl";
 
-const CardComponent = ({ cardImage = "../../images/flip-flop-frugal.svg", cardBodyTitle = "Frugal", cardBodySubtitle = "$10/mo", cardBodyText, buttonText = "Sign Up", outline }) => {
+const CardComponent = ({ handleClick, cardImage = "../../images/flip-flop-frugal.svg", cardBodyTitle = "Frugal", cardBodySubtitle = "$10/mo", cardBodyText, buttonText = "Sign Up", outline }) => {
   return (
     <Card className={outline ? "mb-5 mb-md-0" : "mb-5 mb-md-0 border-0"}>
       <CardImg src={cardImage} alt={cardBodyTitle} className="w-50 m-auto" />
@@ -10,8 +10,9 @@ const CardComponent = ({ cardImage = "../../images/flip-flop-frugal.svg", cardBo
         <CardSubtitle tag="h4" className="mb-2 text-muted">
           {cardBodySubtitle}
         </CardSubtitle>
-        <CardText>{cardBodyText}</CardText>
-        <Button color="primary">{buttonText}</Button>
+        <Button color="primary" onClick={handleClick}>
+          Sign Up
+        </Button>
       </CardBody>
     </Card>
   );
