2120028265807e4c7866ffef37359a5bce087016
Robert Turner <robturneris@gmail.com>
2022-01-16 00:27:57 -0500

    Removed unnecessary props

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -1,7 +1,7 @@
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Button } from "reactstrap";
 import { baseUrl } from "../shared/baseUrl";
 
-const CardComponent = ({ handleClick, cardImage = "../../images/flip-flop-frugal.svg", cardBodyTitle = "Frugal", cardBodySubtitle = "$10/mo", cardBodyText, buttonText = "Sign Up", outline }) => {
+const CardComponent = ({ handleClick, outline }) => {
   return (
     <Card className={outline ? "mb-5 mb-md-0" : "mb-5 mb-md-0 border-0"}>
       <CardImg src={cardImage} alt={cardBodyTitle} className="w-50 m-auto" />
