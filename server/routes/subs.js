f839ba0437c3fe97ec427bf6ce6b1ceecd082a27
Robert Turner <robturneris@gmail.com>
2022-01-15 21:18:22 -0500

    Fix prices controller import

--- server/routes/subs.js
+++ server/routes/subs.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-import { prices } from "../controllers/auth";
+import { prices } from "../controllers/subs";
 
 router.get("/prices", prices);
 
