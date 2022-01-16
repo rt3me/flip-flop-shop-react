771ea15f42ccee3d9fb8c1bb3e2c1a8649574847
Robert Turner <robturneris@gmail.com>
2022-01-16 15:28:59 -0500

    Key in outer most element, add col-md div

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -6,22 +6,24 @@ const CardComponent = ({ price, handleSubscription, outline }) => {
   console.log("Price logged here:", price);
 
   return (
-    <Card className={outline ? "mb-5 mb-md-0" : "mb-5 mb-md-0 border-0"}>
-      <CardImg src={price.image} alt={price.nickname} className="w-50 m-auto" />
-      <CardBody>
-        <CardTitle tag="h3">{price.nickname}</CardTitle>
-        <CardSubtitle tag="h4" className="mb-2 text-muted">
-          {(price.unit_amount / 100).toLocaleString("en-US", {
-            style: "currency",
-            currency: "USD",
-          })}
-          <small className="text-muted fw-light">/mo</small>
-        </CardSubtitle>
-        <Link to="/register">
-          <Button color="primary" /* onClick={() => handleSubscription(price)} */>Sign Up</Button>
-        </Link>
-      </CardBody>
-    </Card>
+    <div className="col-md">
+      <Card className={outline ? "mb-5 mb-md-0" : "mb-5 mb-md-0 border-0"}>
+        <CardImg src={price.image} alt={price.nickname} className="w-50 m-auto" />
+        <CardBody>
+          <CardTitle tag="h3">{price.nickname}</CardTitle>
+          <CardSubtitle tag="h4" className="mb-2 text-muted">
+            {(price.unit_amount / 100).toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}
+            <small className="text-muted fw-light">/mo</small>
+          </CardSubtitle>
+          <Link to="/register">
+            <Button color="primary" /* onClick={() => handleSubscription(price)} */>Sign Up</Button>
+          </Link>
+        </CardBody>
+      </Card>
+    </div>
   );
 };
 
