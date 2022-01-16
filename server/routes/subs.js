bda8fb556ca249fe3418b23043cdf167b13e253e
Robert Turner <robturneris@gmail.com>
2022-01-16 16:43:23 -0500

    Imported createSubscription

--- server/routes/subs.js
+++ server/routes/subs.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-import { prices } from "../controllers/subs";
+import { prices, createSubscription } from "../controllers/subs";
 
 router.get("/prices", prices);
 router.post("/create-subscription", requireSignin, createSubscription);
