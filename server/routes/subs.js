ca6e042e447e1d6180ef39682c3ee39bf00900f6
Robert Turner <robturneris@gmail.com>
2022-01-16 16:41:30 -0500

    Created create subscription route

--- server/routes/subs.js
+++ server/routes/subs.js
@@ -5,5 +5,6 @@ const router = express.Router();
 import { prices } from "../controllers/subs";
 
 router.get("/prices", prices);
+router.post("/create-subscription", requireSignin, createSubscription);
 
 module.exports = router;
