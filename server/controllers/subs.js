96f51494373690ee0e96fff67879a943dc8e1c88
Robert Turner <robturneris@gmail.com>
2022-01-16 18:09:06 -0500

    Import User model and create Stripe subscription

--- server/controllers/subs.js
+++ server/controllers/subs.js
@@ -1,3 +1,4 @@
+import User from "../models/user";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export const prices = async (req, res) => {
@@ -8,4 +9,25 @@ export const prices = async (req, res) => {
 
 export const createSubscription = async (req, res) => {
   // console.log(req.body);
+  try {
+    const user = await User.findById(req.user._id);
+
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: req.body.priceId,
+          quantity: 1,
+        },
+      ],
+      customer: user.stripe_customer_id,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+    console.log("checkout session", session);
+    res.json(session.url);
+  } catch (err) {
+    console.log(err);
+  }
 };
