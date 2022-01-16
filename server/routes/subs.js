d41beb1987f8cafce8a25ff27ad2607e3debb2a8
Robert Turner <robturneris@gmail.com>
2022-01-16 16:47:12 -0500

    Imported requireSignin middleware

--- server/routes/subs.js
+++ server/routes/subs.js
@@ -3,6 +3,7 @@ import express from "express";
 const router = express.Router();
 
 import { prices, createSubscription } from "../controllers/subs";
+import { requireSignin } from "../middlewares";
 
 router.get("/prices", prices);
 router.post("/create-subscription", requireSignin, createSubscription);
