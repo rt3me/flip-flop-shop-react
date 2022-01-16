4fcd23e171cbb84a288c8ac22b46b3470fef5985
Robert Turner <robturneris@gmail.com>
2022-01-16 17:51:45 -0500

    Post to add subscription and open window for signup

--- client/src/components/HomeComponent.jsx
+++ client/src/components/HomeComponent.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { UserContext } from "../context";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import PageSectionLayout from "./PageSectionLayout";
 import PriceCardComponent from "./PriceCardComponent";
 
 function Home(props) {
+  const [state, setState] = useContext(UserContext);
   const [prices, setPrices] = useState([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchPrices();
@@ -38,6 +43,14 @@ function Home(props) {
   const handleClick = async (e, price) => {
     e.preventDefault();
     // console.log("Plan clicked, price.id:", price.id);
+    if (state && state.token) {
+      const { data } = await axios.post("/create-subscription", {
+        priceId: price.id,
+      });
+      window.open(data);
+    } else {
+      navigate("/register");
+    }
   };
 
   return (
