75396ec4c1a9165f9b5b8142909e8e1d0d29286d
Robert Turner <robturneris@gmail.com>
2022-01-17 17:27:43 -0500

    New text for FrugalPlan component

--- client/src/components/BasicPlanComponent.jsx
+++ client/src/components/BasicPlanComponent.jsx
@@ -28,8 +28,8 @@ const Basic = ({ match }) => {
     <React.Fragment>
       <div className="container-fluid">
         <div className="row py-5 bg-light text-center">
-          <h1 className="display-4 fw-bold">BASIC</h1>
-          <p className="lead">Here are your 5 exclusive stocks of this month</p>
+          <h1 className="display-4 fw-bold">Frugal</h1>
+          <p className="lead">Flip flops for the frugal man</p>
         </div>
       </div>
 
@@ -37,21 +37,12 @@ const Basic = ({ match }) => {
         <div className="row">
           <div className="col-md-8 p-5 rounded bg-dark text-light">
             <ul className="lead">
-              <li>Tesla</li>
-              <li>Microsoft</li>
-              <li>PayPal</li>
-              <li>Square</li>
-              <li>Alibaba</li>
+              <li>Good looks built-in</li>
+              <li>Jealous looks from the over-spenders</li>
+              <li>Good times all-around</li>
+              <li>Plenty more good times with the bucks you saved</li>
+              <li>What's next?</li>
             </ul>
-          </div>
-
-          <div className="col-md-4">
-            <h4>Market analysis</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ratione pariatur ab unde voluptatem ea, quae veniam aperiam sint porro aliquid animi eveniet, culpa id reiciendis vel nihil veritatis qui.</p>
-            <h4>Email support</h4>
-            <p>subscriptions@domain.com</p>
-            <h4>Help center</h4>
-            1300 123 456
           </div>
         </div>
       </div>
