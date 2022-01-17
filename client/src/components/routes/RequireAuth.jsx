a7527d4ae239de6876016ba6a324b7792083af7c
Robert Turner <robturneris@gmail.com>
2022-01-17 04:40:20 -0500

    Added console.logs

--- client/src/components/routes/RequireAuth.jsx
+++ client/src/components/routes/RequireAuth.jsx
@@ -1,10 +1,14 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { UserContext } from "../../context";
 
-const RequireAuth = ({ children }) => {
+const RequireAuth = ({ children, ...rest }) => {
   const [state, setState] = useContext(UserContext);
   const location = useLocation();
+
+  console.log("RequireAuth state:", state);
+  console.log("RequireAuth location.pathname:", location.pathname);
+  console.log("RequireAuth ...rest:", { ...rest });
 
   return state === true ? children : <Navigate to="/login" replace state={{ path: location.pathname }} />;
 };
