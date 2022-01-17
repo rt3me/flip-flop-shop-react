4bbeb29a4c252d9f793e1e77107a8b08c1d8720a
Robert Turner <robturneris@gmail.com>
2022-01-17 03:55:00 -0500

    Added subscriptions controller

--- server/controllers/subs.js
+++ server/controllers/subs.js
@@ -55,3 +55,19 @@ export const subscriptionStatus = async (req, res) => {
     console.log(err);
   }
 };
+
+export const subscriptions = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    const subscriptions = await stripe.subscriptions.list({
+      customer: user.stripe_customer_id,
+      status: "all",
+      expand: ["data.default_payment_method"],
+    });
+
+    res.json(subscriptions);
+  } catch (err) {
+    console.log(err);
+  }
+};
