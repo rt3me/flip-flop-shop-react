38a775f5d02848a97107ebd3d2cd9f9b357ee2a7
Robert Turner <robturneris@gmail.com>
2022-01-17 03:53:03 -0500

    Added subscriptions server route

--- server/routes/subs.js
+++ server/routes/subs.js
@@ -2,11 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-import { prices, createSubscription, subscriptionStatus } from "../controllers/subs";
+import { prices, createSubscription, subscriptionStatus, subscriptions } from "../controllers/subs";
 import { requireSignin } from "../middlewares";
 
 router.get("/prices", prices);
 router.post("/create-subscription", requireSignin, createSubscription);
 router.get("/subscription-status", requireSignin, subscriptionStatus);
+router.get("/subscriptions", requireSignin, subscriptions);
 
 module.exports = router;
