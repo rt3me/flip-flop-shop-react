8ae2960c8eecd607a72f461a984921c9dcfc6e41
Robert Turner <robturneris@gmail.com>
2022-01-16 20:44:57 -0500

    Routes begin with /

--- client/src/components/MainComponent.jsx
+++ client/src/components/MainComponent.jsx
@@ -24,12 +24,12 @@ class Main extends Component {
         />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="checkout/success" element={<CheckoutSuccess />} />
-          <Route path="checkout/cancel" element={<CheckoutCancel />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/checkout/success" element={<CheckoutSuccess />} />
+          <Route path="/checkout/cancel" element={<CheckoutCancel />} />
         </Routes>
         <Footer />
       </div>
