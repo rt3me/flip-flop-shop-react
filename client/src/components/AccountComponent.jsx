cbfbddea59a8f9e92ac1c608f07fb2b6d376954c
Robert Turner <robturneris@gmail.com>
2022-01-17 04:41:23 -0500

    Get subscription status in Account component

--- client/src/components/AccountComponent.jsx
+++ client/src/components/AccountComponent.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { UserOutlined } from "@ant-design/icons";
+import axios from "axios";
+import { UserContext } from "../context";
 
 const Account = ({ history }) => {
+  const [state, setState] = useContext(UserContext);
+  const [subscriptions, setSubscriptions] = useState([]);
+
+  useEffect(() => {
+    const getSubscriptions = async () => {
+      const { data } = await axios.get("/subscriptions");
+      console.log("subs => ", data);
+      setSubscriptions(data.data);
+    };
+
+    if (state && state.token) getSubscriptions();
+  }, [state && state.token]);
+
   return (
     <div className="container">
       <div className="row">
         <UserOutlined className="display-4" />
         <h1>Account</h1>
         <p className="lead pb-4">Subscription status</p>
+        <pre>{JSON.stringify(subscriptions, null, 4)}</pre>
       </div>
     </div>
   );
