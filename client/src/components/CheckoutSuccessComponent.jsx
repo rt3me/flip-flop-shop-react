0f2b1bd00acce420c4e9e86d489f89d59fc12a25
Robert Turner <robturneris@gmail.com>
2022-01-17 05:33:37 -0500

    Save subscription data to local storage on checkout success

--- client/src/components/CheckoutSuccessComponent.jsx
+++ client/src/components/CheckoutSuccessComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { SyncOutlined } from "@ant-design/icons";
@@ -15,12 +15,20 @@ const CheckoutSuccess = () => {
       if (data && data.length === 0) {
         navigate("/");
       } else {
-        navigate("/account");
+        // update user in local storage
+        const auth = JSON.parse(localStorage.getItem("auth"));
+        auth.user = data;
+        localStorage.setItem("auth", JSON.stringify(auth));
+        // update user in context
+        setState(auth);
+        setTimeout(() => {
+          navigate("/account");
+        }, 1000);
       }
     };
 
     if (state && state.token) getSubscriptionStatus();
-  }, [state && state.token]);
+  }, [state && state.token, state, setState, navigate]);
 
   return (
     <div className="d-flex justify-content-center fw-bold" style={{ height: "90vh" }}>
