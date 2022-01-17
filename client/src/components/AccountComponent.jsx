ddf57cc502bdbcc446d85c2fe3bf7461284adef4
Robert Turner <robturneris@gmail.com>
2022-01-17 05:00:49 -0500

    Format subscription information

--- client/src/components/AccountComponent.jsx
+++ client/src/components/AccountComponent.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { UserOutlined } from "@ant-design/icons";
 import axios from "axios";
 import { UserContext } from "../context";
+import moment from "moment";
 
 const Account = ({ history }) => {
   const [state, setState] = useContext(UserContext);
@@ -23,7 +24,34 @@ const Account = ({ history }) => {
         <UserOutlined className="display-4" />
         <h1>Account</h1>
         <p className="lead pb-4">Subscription status</p>
-        <pre>{JSON.stringify(subscriptions, null, 4)}</pre>
+        {/* <pre>{JSON.stringify(subscriptions, null, 4)}</pre> */}
+      </div>
+
+      <div className="row">
+        {subscriptions &&
+          subscriptions.map((sub) => (
+            <div key={sub.id}>
+              <section>
+                <hr />
+                <h4 className="fw-bold">{sub.plan.nickname}</h4>
+                <h5>
+                  {(sub.plan.amount / 100).toLocaleString("en-US", {
+                    style: "currency",
+                    currency: sub.plan.currency,
+                  })}
+                </h5>
+                <p>Status: {sub.status}</p>
+                <p>Card last 4 digit: {sub.default_payment_method.card.last4}</p>
+                <p>
+                  Current period end:{" "}
+                  {moment(sub.current_period_end * 1000)
+                    .format("dddd, MMMM Do YYYY h:mm:ss a")
+                    .toString()}
+                </p>
+                <button className="btn btn-outline-danger">Access</button> <button className="btn btn-outline-warning">Manage Subscription</button>
+              </section>
+            </div>
+          ))}
       </div>
     </div>
   );
