833e5265afdf1e9b34d36c86ccb2457a2cba8d53
Robert Turner <robturneris@gmail.com>
2022-01-16 23:38:05 -0500

    Added useEffect to get subscription status in checkout success component

--- client/src/components/CheckoutSuccessComponent.jsx
+++ client/src/components/CheckoutSuccessComponent.jsx
@@ -2,7 +2,21 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { SyncOutlined } from "@ant-design/icons";
 
-const CheckoutSuccess = () => {
+const StripeSuccess = ({ history }) => {
+  useEffect(() => {
+    const getSubscriptionStatus = async () => {
+      const { data } = await axios.get("/subscription-status");
+      console.log("SUBSCRIPTION STATUS => ", data);
+      if (data && data.length === 0) {
+        history.push("/");
+      } else {
+        history.push("/account");
+      }
+    };
+
+    getSubscriptionStatus();
+  }, []);
+
   return (
     <div className="d-flex justify-content-center fw-bold" style={{ height: "90vh" }}>
       <div className="d-flex align-items-center">
@@ -12,4 +26,4 @@ const CheckoutSuccess = () => {
   );
 };
 
-export default CheckoutSuccess;
+export default StripeSuccess;
