d1f7f52787b8a42234c9825c60d99df2aecd1f54
Robert Turner <robturneris@gmail.com>
2022-01-21 17:09:15 -0500

    Added manage server route

--- server/routes/subs.js
+++ server/routes/subs.js
@@ -2,12 +2,13 @@ import express from "express";
 
 const router = express.Router();
 
-import { prices, createSubscription, subscriptionStatus, subscriptions } from "../controllers/subs";
+import { prices, createSubscription, subscriptionStatus, subscriptions, managePortal } from "../controllers/subs";
 import { requireSignin } from "../middlewares";
 
 router.get("/prices", prices);
 router.post("/create-subscription", requireSignin, createSubscription);
 router.get("/subscription-status", requireSignin, subscriptionStatus);
 router.get("/subscriptions", requireSignin, subscriptions);
+router.get("/manage", requireSignin, managePortal);
 
 module.exports = router;
