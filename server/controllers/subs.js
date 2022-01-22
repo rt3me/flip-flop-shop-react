d34bc85385a8ad3dd2b1ca4245b8dd235e703786
Robert Turner <robturneris@gmail.com>
2022-01-22 13:14:41 -0500

    Add address information to subscription

--- server/controllers/subs.js
+++ server/controllers/subs.js
@@ -13,8 +13,54 @@ export const createSubscription = async (req, res) => {
     const user = await User.findById(req.user._id);
 
     const session = await stripe.checkout.sessions.create({
-      mode: "subscription",
       payment_method_types: ["card"],
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA"],
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 0,
+              currency: "usd",
+            },
+            display_name: "Free shipping",
+            // Delivers between 5-7 business days
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 5,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 7,
+              },
+            },
+          },
+        },
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 1500,
+              currency: "usd",
+            },
+            display_name: "Next day air",
+            // Delivers in exactly 1 business day
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 1,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 1,
+              },
+            },
+          },
+        },
+      ],
       line_items: [
         {
           price: req.body.priceId,
@@ -22,9 +68,11 @@ export const createSubscription = async (req, res) => {
         },
       ],
       customer: user.stripe_customer_id,
+      mode: "subscription",
       success_url: process.env.STRIPE_SUCCESS_URL,
       cancel_url: process.env.STRIPE_CANCEL_URL,
     });
+
     console.log("checkout session", session);
     res.json(session.url);
   } catch (err) {
