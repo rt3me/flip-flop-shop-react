0475e652afa74f181f575b6672f2c3fe20c40335
Robert Turner <robturneris@gmail.com>
2022-01-28 22:14:21 -0500

    Fonts and layout

--- client/src/components/PageSectionLayout.jsx
+++ client/src/components/PageSectionLayout.jsx
@@ -4,8 +4,8 @@ const PageSectionLayout = ({ sectionTitle = "Page Section Title", sectionSubtitl
   return (
     <div className="container-fluid d-flex justify-content-center pt-5 pb-4">
       <div className="container">
-        <div className="row col-md-6 offset-md-3 text-center">
-          <h2 className="fw-bold">{sectionTitle}</h2>
+        <div className="row col-md-8 offset-md-2 text-center">
+          <h2>{sectionTitle}</h2>
           <p className="lead pb-4">{sectionSubtitle}</p>
         </div>
         <div className="row mb-4 text-center">{children}</div>
