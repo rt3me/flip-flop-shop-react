82a5c261210925d2009127ca83ddea274b6d87b6
Robert Turner <robturneris@gmail.com>
2022-01-31 17:37:58 -0500

    New footer

--- client/src/components/FooterComponent.jsx
+++ client/src/components/FooterComponent.jsx
@@ -3,35 +3,118 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="site-footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-3 col-sm-3 offset-sm-2">
-            <h5>Links</h5>
-            <ul className="list-unstyled">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="col-sm-3">
-            <h5>Contact</h5>
-            <ul className="list-unstyled">
-              <li>
-                <a href="tel:+18103995911">810-399-5911</a>
-              </li>
-              <li>
-                <a href="mailto:hello@flipflopsurf.shop">hello@flipflopsurf.shop</a>
-              </li>
-            </ul>
+    <footer className="text-center text-lg-start text-white">
+      <section className="d-flex justify-content-between p-4">
+        <div className="me-5">
+          <span>Get connected with us on social networks:</span>
+        </div>
+
+        <div>
+          <Link to="" className="text-white me-4">
+            <i className="fa fa-facebook-f"></i>
+          </Link>
+          <Link to="" className="text-white me-4">
+            <i className="fa fa-twitter"></i>
+          </Link>
+          <Link to="" className="text-white me-4">
+            <i className="fa fa-google"></i>
+          </Link>
+          <Link to="" className="text-white me-4">
+            <i className="fa fa-instagram"></i>
+          </Link>
+          <Link to="" className="text-white me-4">
+            <i className="fa fa-linkedin"></i>
+          </Link>
+          <Link to="" className="text-white me-4">
+            <i className="fa fa-github"></i>
+          </Link>
+        </div>
+      </section>
+
+      <section>
+        <div className="container text-center text-md-start mt-5">
+          <div className="row mt-3">
+            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold">Company name</h6>
+              <hr className="mb-4 mt-0 d-inline-block mx-auto" />
+              <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+
+            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold">Products</h6>
+              <hr className="mb-4 mt-0 d-inline-block mx-auto" />
+              <p>
+                <Link to="#!" className="text-white">
+                  MDBootstrap
+                </Link>
+              </p>
+              <p>
+                <Link to="#!" className="text-white">
+                  MDWordPress
+                </Link>
+              </p>
+              <p>
+                <Link to="#!" className="text-white">
+                  BrandFlow
+                </Link>
+              </p>
+              <p>
+                <Link to="#!" className="text-white">
+                  Bootstrap Angular
+                </Link>
+              </p>
+            </div>
+
+            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold">Useful links</h6>
+              <hr className="mb-4 mt-0 d-inline-block mx-auto" />
+              <p>
+                <Link to="#!" className="text-white">
+                  Your Account
+                </Link>
+              </p>
+              <p>
+                <Link to="#!" className="text-white">
+                  Become an Affiliate
+                </Link>
+              </p>
+              <p>
+                <Link to="#!" className="text-white">
+                  Shipping Rates
+                </Link>
+              </p>
+              <p>
+                <Link to="#!" className="text-white">
+                  Help
+                </Link>
+              </p>
+            </div>
+
+            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <h6 className="text-uppercase fw-bold">Contact</h6>
+              <hr className="mb-4 mt-0 d-inline-block mx-auto" />
+              <p>
+                <i className="fa fa-home mr-3"></i> New York, NY 10012, US
+              </p>
+              <p>
+                <i className="fa fa-envelope mr-3"></i> info@example.com
+              </p>
+              <p>
+                <i className="fa fa-phone mr-3"></i> + 01 234 567 88
+              </p>
+              <p>
+                <i className="fa fa-print mr-3"></i> + 01 234 567 89
+              </p>
+            </div>
           </div>
         </div>
+      </section>
+
+      <div className="text-center p-3">
+        © 2020 Copyright:
+        <Link className="text-white" to="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </Link>
       </div>
     </footer>
   );
