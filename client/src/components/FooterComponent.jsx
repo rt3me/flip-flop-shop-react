5a5ef60b7410653517a103c2158e178c412aee6e
Robert Turner <robturneris@gmail.com>
2022-01-31 19:23:15 -0500

    Footer paragraph

--- client/src/components/FooterComponent.jsx
+++ client/src/components/FooterComponent.jsx
@@ -26,9 +26,9 @@ function Footer() {
         <div className="container text-center text-md-start mt-5">
           <div className="row mt-3">
             <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold">Flip Flop Surf Shop</h6>
+              <h6 className="text-uppercase fw-bold">Flip Flop Shop</h6>
               <hr className="mb-4 mt-0 d-inline-block mx-auto" />
-              <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p>Try a flip flop subscription today. We think you won't regret it! If you do, just cancel at any time.</p>
             </div>
 
             <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
