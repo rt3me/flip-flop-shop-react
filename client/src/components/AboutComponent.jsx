57873ed528e53bd70b78d459aa0aec316b830b6f
Robert Turner <robturneris@gmail.com>
2022-02-22 22:09:00 -0500

    About page updates

--- client/src/components/AboutComponent.jsx
+++ client/src/components/AboutComponent.jsx
@@ -8,7 +8,7 @@ function About() {
         <div className="container">
           <div className="row py-5 text-center">
             <h1 className="display-4 fw-bold">About Us</h1>
-            <p className="lead">Get the skinny on us</p>
+            <p className="lead">Get the skinny</p>
           </div>
         </div>
       </div>
@@ -18,8 +18,9 @@ function About() {
           <div className="col-sm-6">
             <h3>Our Mission</h3>
             <p>
-              We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert
-              wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+              We could ramble on all day about the benefits flip flops provide to society. Instead, we would like to present our argument for not using a hyphen in the word "flip flop". According to{" "}
+              <a href="https://minnesota.publicradio.org/radio/podcasts/grammar_grater/archive/2007/12/06/">Minnesota Public Radio</a>, "Flip-flop is in a family of words that are created by reduplication: that is, the repetition of the same or
+              similar sound. Other words in this family include knick-knack, hodge-podge, shilly-shally and hoity-toity. Like flip-flop, these words are often hyphenated." We simply say, no. Not today. Because... flip flops.
             </p>
           </div>
           <div className="col-sm-6">
@@ -30,13 +31,13 @@ function About() {
               <CardBody>
                 <dl className="row">
                   <dt className="col-6">Founded</dt>
-                  <dd className="col-6">February 3, 2016</dd>
-                  <dt className="col-6">No. of Campsites in 2019</dt>
-                  <dd className="col-6">563</dd>
-                  <dt className="col-6">No. of Reviews in 2019</dt>
-                  <dd className="col-6">4388</dd>
-                  <dt className="col-6">Employees</dt>
+                  <dd className="col-6">February 22, 2022</dd>
+                  <dt className="col-6">Flip Flops In Existence</dt>
                   <dd className="col-6">42</dd>
+                  <dt className="col-6">Flip Flops Worn Now</dt>
+                  <dd className="col-6">11'dy 7</dd>
+                  <dt className="col-6">Toes Freed By Flip Flops</dt>
+                  <dd className="col-6">Millions</dd>
                 </dl>
               </CardBody>
             </Card>
@@ -45,10 +46,10 @@ function About() {
             <Card className="mt-3 bg-light">
               <CardBody>
                 <blockquote className="blockquote">
-                  <p className="mb-0">I will not follow where the path may lead, but I will go where there is no path, and I will leave a trail.</p>
-                  <footer className="blockquote-footer">
-                    Muriel Strode, <cite title="Source Title">"Wind-Wafted Wild Flowers" - The Open Court, 1903</cite>
-                  </footer>
+                  <p className="mb-0">I love living my life in flip-flops. I met a guy in the islands a while ago who told me he hadn't worn a pair of shoes in three years! I thought, 'Man, that's the life!'.</p>
+                  <div className="blockquote-footer">
+                    Kenny Chesney, <cite title="Source Title">Somewhere, some time...</cite>
+                  </div>
                 </blockquote>
               </CardBody>
             </Card>
