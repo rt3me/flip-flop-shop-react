3bcb764f0ac509b764b803bf67528b48a97f13b6
Robert Turner <robturneris@gmail.com>
2022-02-22 21:36:47 -0500

    Increaded padding

--- client/src/components/PageSectionLayout.jsx
+++ client/src/components/PageSectionLayout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageSectionLayout = ({ sectionTitle, sectionSubtitle, children }) => {
   return (
     <div className="container-fluid d-flex justify-content-center pt-5 pb-4">
-      <div className="container">
+      <div className="container py-5">
         <div className="row col-md-8 offset-md-2 text-center">
           {sectionTitle ? <h2>{sectionTitle}</h2> : ""}
           {sectionSubtitle ? <p className="lead pb-4">{sectionSubtitle}</p> : ""}
