97f3dbebb2114e93a301f0dbe3ac3538b2dff2db
Robert Turner <robturneris@gmail.com>
2022-02-22 21:32:38 -0500

    Page section optional title and subtitle

--- client/src/components/PageSectionLayout.jsx
+++ client/src/components/PageSectionLayout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const PageSectionLayout = ({ sectionTitle = "Page Section Title", sectionSubtitle = "Example section subtitle with medium length text here", children }) => {
+const PageSectionLayout = ({ sectionTitle, sectionSubtitle, children }) => {
   return (
     <div className="container-fluid d-flex justify-content-center pt-5 pb-4">
       <div className="container">
         <div className="row col-md-8 offset-md-2 text-center">
-          <h2>{sectionTitle}</h2>
-          <p className="lead pb-4">{sectionSubtitle}</p>
+          {sectionTitle ? <h2>{sectionTitle}</h2> : ""}
+          {sectionSubtitle ? <p className="lead pb-4">{sectionSubtitle}</p> : ""}
         </div>
         <div className="row mb-4 text-center">{children}</div>
       </div>
